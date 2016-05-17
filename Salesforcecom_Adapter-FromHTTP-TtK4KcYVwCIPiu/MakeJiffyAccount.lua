5b66c440e47f46726125cbf4aab4c39adfc9b339
admin <admin>
2016-05-17 20:48:34 +0000

    Export from Highrise 2 Server: More comments in the examples.

--- Salesforcecom_Adapter-FromHTTP-TtK4KcYVwCIPiu/MakeJiffyAccount.lua
+++ Salesforcecom_Adapter-FromHTTP-TtK4KcYVwCIPiu/MakeJiffyAccount.lua
@@ -1,16 +1,19 @@
-
 local function MakeJiffyAccount(C)
-      local AccountName = "Jiffy Dry Cleaning"
+   local AccountName = "Jiffy Dry Cleaning"
+   -- For this query we are disabling caching of the results - hence
+   -- cache_time = 0
    local JiffyAccount = C:accountList{where="Name = '"..AccountName.."'",
        cache_time=0}
    if #JiffyAccount.account == 0 then
-      -- We could not find the 
+      -- We could not find the Jiffy Account so lets create a new record
+      -- This method gives us an empty record to populate
       JiffyAccount = C:accountNew()
    end
    
    A = JiffyAccount.account[1]
    A.Name = AccountName
    A.Industry = "Dry Cleaner"
+   -- accountModify is how we actually submit the new or modified account
    local JiffyId = C:accountModify{data=JiffyAccount, live=true}
    C:accountDelete{id=JiffyId}
 end
--- Salesforcecom_Adapter-FromHTTP-TtK4KcYVwCIPiu/ShowUsers.lua
+++ Salesforcecom_Adapter-FromHTTP-TtK4KcYVwCIPiu/ShowUsers.lua
@@ -1,14 +1,16 @@
-
-
 local function ShowUsers(C)
+   -- This example we just display the users of the instance
+   -- of salesforce.com
    local U = C:userList{}
  
    local R = 'List of salesforce users:\n'
+   -- Loop through all the users and append them to a list.
    for i=1, #U.user do
       trace(U.user[i].FirstName.." "..U.user[i].LastName)
       R = R..U.user[i].FirstName.." "..U.user[i].LastName.."\n"
    end
-   
+
+   -- Display the list
    net.http.respond{body=R, entity_type='text/plain'}
 end
 
